6b720305e5600b963b1002d3de5e896bea094c81
Al Maline <amaline@users.noreply.github.com>
2016-10-06 14:24:37 -0400

    fixed interpreter location

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-#!bash
+#!/bin/bash
 (git rev-parse ${GITHUB_RELEASE} || ( git tag ${GITHUB_RELEASE} && git push https://${GITHUB_TOKEN}:@${PROJECT_REPOSITORY} --tags))
 curl -# -XPOST -H "Authorization: ${GITHUB_TOKEN}" -H "Content-Type: application/octet-stream" --data-binary @${CIRCLE_ARTIFACTS}/${ASSET_NAME} https://uploads.github.com/repos/${GITHUB_PROJECT}/releases/`git rev-parse ${GITHUB_RELEASE}`/assets?name=${ASSET_NAME}
